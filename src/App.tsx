c75d4f35266925557275772c359661cb54effc52
Griffin Cox <griffin@ezbot.ai>
2024-11-03 16:21:41 -0600

    with current @clerk/chrome-extension version 1.3.27

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,44 @@
-import "./App.css";
-
-import {
-  SignedIn,
-  SignedOut,
-  SignIn,
-  SignUp,
-  useClerk,
-  useUser,
-  ClerkProvider,
-} from "@clerk/chrome-extension";
-import {
-  useNavigate,
-  Routes,
-  Route,
-  MemoryRouter
-} from "react-router-dom";
+// App.tsx
+import { SignedIn, SignedOut, SignIn, SignUp, ClerkProvider } from '@clerk/chrome-extension';
+import { useNavigate, Routes, Route, MemoryRouter } from 'react-router-dom';
 
 function HelloUser() {
-  const { isSignedIn, user } = useUser();
-  const clerk = useClerk();
-
-  if (!isSignedIn) {
-    return null;
-  }
-
-  return (
-    <>
-      <p>Hi, {user.primaryEmailAddress?.emailAddress}!</p>
-      <p>
-        <button onClick={() => clerk.signOut()}>Sign out</button>
-      </p>
-    </>
-  );
+  return <p>Hello user</p>;
 }
 
-const publishableKey = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY || "";
+const publishableKey = process.env.VITE_CLERK_PUBLISHABLE_KEY || '';
 
 function ClerkProviderWithRoutes() {
   const navigate = useNavigate();
 
   return (
-    <ClerkProvider publishableKey={publishableKey} navigate={(to) => navigate(to)}>
-      <div className="App">
-        <header className="App-header">
-          <p>Welcome to Clerk Chrome Extension Starter!</p>
-          <a
-            className="App-link"
-            href="https://clerk.dev/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn more about Clerk
-          </a>
-        </header>
-        <main className="App-main">
-          <Routes>
-            <Route
-              path="/sign-up/*"
-              element={<SignUp signInUrl="/" />}
-            />
-            <Route path='/' element={
-              <>
-                <SignedIn>
-                  <HelloUser />
-                </SignedIn>
-                <SignedOut>
-                  <SignIn afterSignInUrl="/" signUpUrl="/sign-up" />
-                </SignedOut>
-              </>
-            } />
-          </Routes>
-        </main>
-      </div>
+    <ClerkProvider
+      publishableKey={publishableKey}
+      routerPush={to => navigate(to)}
+      routerReplace={to => navigate(to, { replace: true })}
+    >
+      <Routes>
+        <Route
+          path='/sign-up/*'
+          element={<SignUp signInUrl='/' />}
+        />
+        <Route
+          path='/'
+          element={
+            <>
+              <SignedIn>
+                <HelloUser />
+              </SignedIn>
+              <SignedOut>
+                <SignIn
+                  afterSignInUrl='/'
+                  signUpUrl='/sign-up'
+                />
+              </SignedOut>
+            </>
+          }
+        />
+      </Routes>
     </ClerkProvider>
   );
 }
